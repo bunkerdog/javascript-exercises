9b0ed4c5fe813c4cfac79c8b9c49302449a6b9f5
bunkerdog <franknfurter2002@protonmail.com>
2022-10-08 13:20:43 -0600

    to an array reversed and back to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strArray = Array.from(str);
+    let revStrArr= strArray.reverse();
+    let revStr = revStrArr.join('');
+    return revStr
+
+
 
 };
+console.log()
 
 // Do not edit below this line
 module.exports = reverseString;
