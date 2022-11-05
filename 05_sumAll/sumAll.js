2eecdac5c80334ad60e84a03c80ef703417afdc3
bunkerdog <franknfurter2002@protonmail.com>
2022-11-05 15:00:00 -0600

    create conditions and the iterations

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a > 0 && b > 0 && typeof a === 'number' && typeof b === 'number') {
+        let valora = a;
+        let valorb = b;
+        let total = 0;
+        if (a < b) {
+            for (var i = valora; i <= valorb; i++) {
+                total += i;
+            }
+            return total;
+        } else if (a > b) {
+            for (var i = valorb; i <= valora; i++) {
+                total += i;
+            }
+            return total;
+        }
+    } else {
+        return 'ERROR'
+     }
 
 };
 
